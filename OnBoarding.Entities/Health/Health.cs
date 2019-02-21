ac6c8977aad14adfec85bd60211b5914338d3d06
prabhurajaprabhu <40233019+prabhurajaprabhu@users.noreply.github.com>
2019-02-21 21:06:32 +0530

    Add files via upload

---
+++ OnBoarding.Entities/Health/Health.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnBoarding.Entities
+{
+    public class Health
+    {
+        public int MobileNumber { get; set; }
+
+        public string HospitalName { get; set; }
+
+        public string PatientName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Smoker { get; set; }
+
+        public string KnownDisease { get; set; }
+
+        public DateTime TreatmentDate { get; set; }
+    }
+}
